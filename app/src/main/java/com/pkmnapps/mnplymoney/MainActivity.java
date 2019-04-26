c9fec311ba2deeb3675c104505e5a052a48d08a0
Prerak Mann <mannprerak2@gmail.com>
2019-04-26 21:49:08 +0530

    added basic hosting, advertising and ui to show states

--- app/src/main/java/com/pkmnapps/mnplymoney/MainActivity.java
+++ app/src/main/java/com/pkmnapps/mnplymoney/MainActivity.java
@@ -1,8 +1,19 @@
 package com.pkmnapps.mnplymoney;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.nearby.Nearby;
@@ -21,19 +32,49 @@ import com.google.android.gms.nearby.connection.Strategy;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String SERVICE_ID = "com.pkmnapps.mnplymoney_service_id";
+    private static final int iStartScreen = 0;
+    private static final int iHostScreen = 1;
+    private static final int iJoinScreen = 2;
+    private static final int iGameScreen = 3;
+    private static final int iLoaderScreen = 4;
+
     private String nickname;
+    private TextView idTextView, discoverStatus, advertiseStatus;
+
+    private View startScreen, hostScreen, joinScreen, gameScreen, loaderScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idTextView = findViewById(R.id.idTextView);
+        idTextView.setText(getUserNickname());
+
+        discoverStatus = findViewById(R.id.discoverStatusTextView);
+        advertiseStatus = findViewById(R.id.advertStatusTextView);
+
+        // project uses xml onclicks as well
+
+        startScreen = findViewById(R.id.startLayout);
+        hostScreen = findViewById(R.id.hostLayout);
+        joinScreen = findViewById(R.id.joinLayout);
+        gameScreen = findViewById(R.id.gameLayout);
+        loaderScreen = findViewById(R.id.loaderLayout);
+
+
+        //check for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
     }
 
+    //common for advertisers and discoverers
     private final PayloadCallback payloadCallback = new PayloadCallback() {
         @Override
         public void onPayloadReceived(@NonNull String s, @NonNull Payload payload) {
@@ -46,11 +87,14 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    //common for advertisers and discoverers
     private final ConnectionLifecycleCallback connectionLifecycleCallback = new ConnectionLifecycleCallback() {
         @Override
         public void onConnectionInitiated(@NonNull String s, @NonNull ConnectionInfo connectionInfo) {
+            // connection was requested by discoverer, and needs to be accepted by both now
             Nearby.getConnectionsClient(MainActivity.this)
                     .acceptConnection(s, payloadCallback);
+            stopDiscovery(); //
         }
 
         @Override
@@ -59,14 +103,18 @@ public class MainActivity extends AppCompatActivity {
                 case ConnectionsStatusCodes.STATUS_OK:
                     // We're connected! Can now start sending and receiving data.
                     Toast.makeText(getApplicationContext(), "Connected to: " + s, Toast.LENGTH_SHORT).show();
+                    discoverStatus.setText("Connected to:" + s + "'s Game");
                     break;
                 case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
                     // The connection was rejected by one or both sides.
                     Toast.makeText(getApplicationContext(), "Connection rejected: " + s, Toast.LENGTH_SHORT).show();
+                    discoverStatus.setText("Connection rejected: " + s);
+
                     break;
                 case ConnectionsStatusCodes.STATUS_ERROR:
                     // The connection broke before it was able to be accepted.
                     Toast.makeText(getApplicationContext(), "Connection broke: " + s, Toast.LENGTH_SHORT).show();
+                    discoverStatus.setText("Connection broke: " + s);
                     break;
                 default:
                     // Unknown status code
@@ -76,10 +124,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onDisconnected(@NonNull String s) {
             Toast.makeText(getApplicationContext(), "Disconnected: " + s, Toast.LENGTH_SHORT).show();
+            discoverStatus.setText("...");
         }
     };
 
-    private void startAdvertising() {
+    public void startAdvertising(View view) {
+        showScreen(iLoaderScreen);
         AdvertisingOptions advertisingOptions = new AdvertisingOptions.Builder()
                 .setStrategy(Strategy.P2P_STAR).build();
 
@@ -89,23 +139,28 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
+                        showScreen(iHostScreen);
                         Toast.makeText(getApplicationContext(), "Advertising started", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        showScreen(iStartScreen);
                         Toast.makeText(getApplicationContext(), "Failed to start Advertising: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
-    private void stopAdvertising() {
+    public void stopAdvertising(View view) {
         Nearby.getConnectionsClient(this).stopAdvertising();
         Toast.makeText(getApplicationContext(), "Advertising stopped", Toast.LENGTH_SHORT).show();
     }
 
-    private void startDiscovery() {
+    public void startDiscovery(View view) {
+        showScreen(iLoaderScreen);
+        discoverStatus.setText("...");
+
         DiscoveryOptions discoveryOptions =
                 new DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
         Nearby.getConnectionsClient(this)
@@ -136,20 +191,89 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
+                        showScreen(iJoinScreen);
                         Toast.makeText(getApplicationContext(), "Discovery started", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
+                        showScreen(iStartScreen);
                         Toast.makeText(getApplicationContext(), "Failed to start Discovery: " + e.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
+    private void stopDiscovery() {
+        Nearby.getConnectionsClient(this).stopDiscovery();
+        Toast.makeText(getApplicationContext(), "Discovery stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    public void quitJoining(View view) {
+        stopDiscovery();
+        Nearby.getConnectionsClient(this).stopAllEndpoints();
+        showScreen(iStartScreen);
+    }
+
+    public void quitHosting(View view) {
+        stopAdvertising(null);
+        Nearby.getConnectionsClient(this).stopAllEndpoints();
+        showScreen(iStartScreen);
+    }
+
+    public void startGame(View view) {
+        Nearby.getConnectionsClient(this).stopAdvertising();
+        //TODO-send a payload to start game
+
+        showScreen(iLoaderScreen);
+    }
+
     private String getUserNickname() {
         if (nickname == null)
-            nickname = String.valueOf((int) (Math.random() * 100));
+            nickname = "User " + (int) (Math.random() * 100);
         return nickname;
     }
+
+    public void editUserID(View view) {
+        // get prompts.xml view
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompt, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(true)
+                .setPositiveButton("Set",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (!userInput.getText().toString().equals(""))
+                                    idTextView.setText(userInput.getText());
+                            }
+                        })
+                .create()
+                .show();
+    }
+
+    private void showScreen(int screenNumber) {
+        startScreen.setVisibility(screenNumber == iStartScreen ? View.VISIBLE : View.GONE);
+        hostScreen.setVisibility(screenNumber == iHostScreen ? View.VISIBLE : View.GONE);
+        joinScreen.setVisibility(screenNumber == iJoinScreen ? View.VISIBLE : View.GONE);
+        gameScreen.setVisibility(screenNumber == iGameScreen ? View.VISIBLE : View.GONE);
+        loaderScreen.setVisibility(screenNumber == iLoaderScreen ? View.VISIBLE : View.GONE);
+
+        if (screenNumber == iStartScreen) {
+            //change all textviews to initial state
+            discoverStatus.setText("...");
+        }
+    }
+
 }
